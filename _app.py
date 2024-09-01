76a037d13d8f1fba63299aa900dc7e283367205b
Theo <Theophilus.kattakkaljohn@in.bosch.com>
2024-09-02 02:28:47 +0530

    Updated the html, app code.

--- _app.py
+++ _app.py
@@ -1,25 +1,17 @@
 from flask import Flask, render_template, request
 import pickle
-import pandas as pd
-from sklearn.preprocessing import LabelEncoder
-
 
 app = Flask(__name__)
 
-# Load the trained model and RFE
 model = pickle.load(open('model.pkl', 'rb'))
 with open('label_encoder.pkl', 'rb') as le_file:
-    le = pickle.load(le_file)
+    label_encoder = pickle.load(le_file)
 
-# Define the feature columns (these should match the ones used in training)
-feature_columns = ['Time', 'Day_of_week', 'Age_band_of_driver', 'Sex_of_driver',
-                   'Educational_level', 'Vehicle_driver_relation', 'Driving_experience',
-                   'Type_of_vehicle', 'Owner_of_vehicle', 'Area_accident_occured',
-                   'Lanes_or_Medians', 'Road_allignment', 'Types_of_Junction',
-                   'Road_surface_type', 'Road_surface_conditions', 'Light_conditions',
-                   'Weather_conditions', 'Type_of_collision', 'Vehicle_movement',
-                   'Casualty_class', 'Pedestrian_movement', 'Cause_of_accident']
-
+feature_columns = [
+    'Age_band_of_driver', 'Sex_of_driver', 'Road_surface_conditions', 'Light_conditions',
+    'Weather_conditions', 'Type_of_collision', 'Number_of_vehicles_involved', 'Casualty_class',
+    'Sex_of_casualty', 'Age_band_of_casualty', 'Casualty_severity'
+]
 
 @app.route('/')
 def home():
@@ -28,25 +20,26 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get data from the form
-    data = {col: request.form.get(col, '') for col in feature_columns}
+    data = [request.form.get(col, '') for col in feature_columns]
+    number_of_vehicles_involved = int(data[feature_columns.index('Number_of_vehicles_involved')])
+    data[feature_columns.index('Number_of_vehicles_involved')] = number_of_vehicles_involved
+    le = pickle.load(open('label_encoder.pkl', 'rb'))
+    transformed_data = []
+    for i, col in enumerate(feature_columns):
+        if col in ['Age_band_of_driver', 'Sex_of_driver', 'Road_surface_conditions', 'Light_conditions',
+                   'Weather_conditions', 'Type_of_collision', 'Casualty_class', 'Sex_of_casualty',
+                   'Age_band_of_casualty', 'Casualty_severity']:
+            transformed_data.append(le.fit_transform([data[i]])[0])
+        else:
+            transformed_data.append(data[i])
 
-    # Create a DataFrame from the form data
-    new_data = pd.DataFrame([data], columns=feature_columns)
+    input_array = [transformed_data]
 
-    le_new = LabelEncoder()
+    new_pred = model.predict(input_array)
 
-    for col in ['Age_band_of_driver', 'Sex_of_driver', 'Road_surface_conditions',
-                'Light_conditions', 'Weather_conditions', 'Type_of_collision',
-                'Casualty_class', 'Sex_of_casualty', 'Age_band_of_casualty', 'Casualty_severity']:
-        new_data[col] = le_new.fit_transform(new_data[col])
-
-    new_pred = model.predict(new_data)
-
-    prediction_label = le.inverse_transform(new_pred)[0]
+    prediction_label = label_encoder.inverse_transform(new_pred)[0]
 
     return render_template('result.html', prediction=prediction_label)
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=False)
--- _model.py
+++ _model.py
@@ -1,78 +1,85 @@
 import pandas as pd
-import warnings
-import pickle
 from sklearn.preprocessing import LabelEncoder
 from imblearn.over_sampling import SMOTE
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, ConfusionMatrixDisplay
+from sklearn.model_selection import train_test_split
+import pickle
 
-warnings.filterwarnings('ignore')
-
-# Load and preprocess data
 df = pd.read_csv('RTA_Dataset.csv')
-df.drop(['Time', 'Day_of_week'], axis=1, inplace=True)
 
-# Drop rows with NaNs in specific columns
-df = df.dropna(subset=['Defect_of_vehicle', 'Service_year_of_vehicle', 'Work_of_casuality'])
+df.drop(['Time','Day_of_week'],axis=1,inplace=True)
 
-# Fill remaining NaNs with the mode of each column
-fill_mode_columns = ['Educational_level', 'Vehicle_driver_relation', 'Driving_experience',
-                     'Type_of_vehicle', 'Owner_of_vehicle', 'Area_accident_occured',
-                     'Lanes_or_Medians', 'Road_allignment', 'Types_of_Junction',
-                     'Road_surface_type', 'Type_of_collision', 'Vehicle_movement',
-                     'Fitness_of_casuality']
-for col in fill_mode_columns:
-    df[col] = df[col].fillna(df[col].mode().iloc[0])
+col =['Age_band_of_driver', 'Sex_of_driver',
+       'Educational_level', 'Vehicle_driver_relation', 'Driving_experience',
+       'Type_of_vehicle', 'Owner_of_vehicle', 'Service_year_of_vehicle',
+       'Defect_of_vehicle', 'Area_accident_occured', 'Lanes_or_Medians',
+       'Road_allignment', 'Types_of_Junction', 'Road_surface_type',
+       'Road_surface_conditions', 'Light_conditions', 'Weather_conditions',
+       'Type_of_collision', 'Number_of_vehicles_involved',
+       'Number_of_casualties', 'Vehicle_movement', 'Casualty_class',
+       'Sex_of_casualty', 'Age_band_of_casualty', 'Casualty_severity',
+       'Work_of_casuality', 'Fitness_of_casuality', 'Pedestrian_movement',
+       'Cause_of_accident', 'Accident_severity']
 
-# Label encoding
-columns_to_encode = ['Age_band_of_driver', 'Sex_of_driver', 'Educational_level',
-                     'Vehicle_driver_relation', 'Driving_experience', 'Type_of_vehicle',
-                     'Owner_of_vehicle', 'Service_year_of_vehicle', 'Defect_of_vehicle',
-                     'Area_accident_occured', 'Lanes_or_Medians', 'Road_allignment',
-                     'Types_of_Junction', 'Road_surface_type', 'Road_surface_conditions',
-                     'Light_conditions', 'Weather_conditions', 'Type_of_collision',
-                     'Vehicle_movement', 'Casualty_class', 'Sex_of_casualty',
-                     'Age_band_of_casualty', 'Casualty_severity', 'Work_of_casuality',
-                     'Fitness_of_casuality', 'Pedestrian_movement', 'Cause_of_accident',
-                     'Accident_severity']
+df = df.dropna(subset=['Defect_of_vehicle'])
+df = df.dropna(subset=['Service_year_of_vehicle'])
+df = df.dropna(subset=['Work_of_casuality'])
+
+df['Educational_level'] = df['Educational_level'].fillna(df['Educational_level'].mode().iloc[0])
+df['Vehicle_driver_relation'] = df['Vehicle_driver_relation'].fillna(df['Vehicle_driver_relation'].mode().iloc[0])
+df['Driving_experience'] = df['Driving_experience'].fillna(df['Driving_experience'].mode().iloc[0])
+df['Type_of_vehicle'] = df['Type_of_vehicle'].fillna(df['Type_of_vehicle'].mode().iloc[0])
+df['Owner_of_vehicle'] = df['Owner_of_vehicle'].fillna(df['Owner_of_vehicle'].mode().iloc[0])
+df['Area_accident_occured'] = df['Area_accident_occured'].fillna(df['Area_accident_occured'].mode().iloc[0])
+df['Lanes_or_Medians'] = df['Lanes_or_Medians'].fillna(df['Lanes_or_Medians'].mode().iloc[0])
+df['Road_allignment'] = df['Road_allignment'].fillna(df['Road_allignment'].mode().iloc[0])
+df['Types_of_Junction'] = df['Types_of_Junction'].fillna(df['Types_of_Junction'].mode().iloc[0])
+df['Road_surface_type'] = df['Road_surface_type'].fillna(df['Road_surface_type'].mode().iloc[0])
+df['Type_of_collision'] = df['Type_of_collision'].fillna(df['Type_of_collision'].mode().iloc[0])
+df['Vehicle_movement'] = df['Vehicle_movement'].fillna(df['Vehicle_movement'].mode().iloc[0])
+df['Fitness_of_casuality'] = df['Fitness_of_casuality'].fillna(df['Fitness_of_casuality'].mode().iloc[0])
+
+
+columns = ['Age_band_of_driver', 'Sex_of_driver', 'Educational_level',
+       'Vehicle_driver_relation', 'Driving_experience', 'Type_of_vehicle',
+       'Owner_of_vehicle', 'Service_year_of_vehicle', 'Defect_of_vehicle',
+       'Area_accident_occured', 'Lanes_or_Medians', 'Road_allignment',
+       'Types_of_Junction', 'Road_surface_type', 'Road_surface_conditions',
+       'Light_conditions', 'Weather_conditions', 'Type_of_collision',
+       'Vehicle_movement', 'Casualty_class', 'Sex_of_casualty',
+       'Age_band_of_casualty', 'Casualty_severity', 'Work_of_casuality',
+       'Fitness_of_casuality', 'Pedestrian_movement', 'Cause_of_accident',
+       'Accident_severity']
 
 le = LabelEncoder()
-for col in columns_to_encode:
-    df[col] = le.fit_transform(df[col])
+for i in columns:
+    df[i] = le.fit_transform(df[i])
 
 with open('label_encoder.pkl', 'wb') as le_file:
     pickle.dump(le, le_file)
 
-# Drop less important columns
-columns_to_drop = ['Educational_level', 'Vehicle_driver_relation', 'Driving_experience',
-                   'Type_of_vehicle', 'Owner_of_vehicle', 'Service_year_of_vehicle',
-                   'Defect_of_vehicle', 'Area_accident_occured', 'Lanes_or_Medians',
-                   'Road_allignment', 'Types_of_Junction', 'Road_surface_type',
-                   'Number_of_casualties', 'Vehicle_movement', 'Work_of_casuality',
-                   'Fitness_of_casuality', 'Pedestrian_movement', 'Cause_of_accident']
-df.drop(columns=columns_to_drop, axis=1, inplace=True)
+colum = ['Educational_level','Vehicle_driver_relation', 'Driving_experience', 'Type_of_vehicle',
+       'Owner_of_vehicle', 'Service_year_of_vehicle', 'Defect_of_vehicle',
+       'Area_accident_occured', 'Lanes_or_Medians', 'Road_allignment',
+       'Types_of_Junction', 'Road_surface_type','Number_of_casualties',
+       'Vehicle_movement','Work_of_casuality','Fitness_of_casuality', 'Pedestrian_movement', 'Cause_of_accident']
 
-# Split data into features and target
+for i in colum:
+    df.drop(i,axis=1,inplace=True)
+
 X = df.drop('Accident_severity', axis=1)
 y = df['Accident_severity']
 
-# Handle class imbalance using SMOTE
-sm = SMOTE(random_state=10)
-X_resampled, y_resampled = sm.fit_resample(X, y)
+sm=SMOTE(random_state=10)
+x_new,y_new=sm.fit_resample(X,y)
 
-# Split data into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=0.3, random_state=1)
+X_train,X_test,y_train,y_test=train_test_split(x_new,y_new,test_size=0.3,random_state=1)
 
-# Train and evaluate models
-rf = RandomForestClassifier()
-
-with open('model.pkl', 'wb') as model_file:
-    pickle.dump(rf, model_file)
+rf = RandomForestClassifier().predict()
 
 rf.fit(X_train, y_train)
 y_pred = rf.predict(X_test)
 
-print("RandomForestClassifier Accuracy:", accuracy_score(y_test, y_pred))
-ConfusionMatrixDisplay.from_predictions(y_test, y_pred)
+with open('model.pkl', 'wb') as model_file:
+    pickle.dump(rf, model_file)
 
